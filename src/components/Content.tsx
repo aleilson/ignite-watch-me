5db76473d37810b3e1905d693f2988ec6a23bc0f
Aleilson <aleilsoncerqueira@gmail.com>
2021-09-21 09:53:47 -0300

    finalized adjustments to optimize components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,38 +1,34 @@
-import { MovieCard } from "./MovieCard";
+import { MovieCard } from "../components/MovieCard";
+
+import { GenreResponseProps, MovieProps } from "../types";
+
+import "../styles/content.scss";
 
 interface ContentProps {
-  selectedGenre: {
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
-  };
-
-  movies: Array<{
-    imdbID: string;
-    Title: string;
-    Poster: string;
-    Ratings: Array<{
-      Source: string;
-      Value: string;
-    }>;
-    Runtime: string;
-  }>;
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
 }
 
-export function Content({ selectedGenre, movies }: ContentProps) {
+export function Content({ movies, selectedGenre }: ContentProps) {
   return (
     <div className="container">
       <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
       </header>
-
       <main>
         <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          {movies.map((movie) => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
           ))}
         </div>
       </main>
     </div>
-  )
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,36 +1,37 @@
-import { Button } from "./Button";
+import { Button } from "../components/Button";
+
+import { GenreResponseProps } from "../types";
+
+import "../styles/sidebar.scss";
 
 interface SideBarProps {
-  genres: Array<{
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
-  }>;
+  genres: GenreResponseProps[];
   selectedGenreId: number;
-  buttonClickCallback: (args: any) => void;
+  handleClickButton: (id: number) => void;
 }
 
 export function SideBar({
   genres,
   selectedGenreId,
-  buttonClickCallback
+  handleClickButton,
 }: SideBarProps) {
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
+        {genres.map((genre) => (
           <Button
-            key={String(genre.id)}
+            id={String(genre.id)}
             title={genre.title}
             iconName={genre.name}
-            onClick={() => buttonClickCallback(genre.id)}
+            onClick={() => handleClickButton(genre.id)}
             selected={selectedGenreId === genre.id}
           />
         ))}
       </div>
-
     </nav>
-  )
+  );
 }
